ade754d0fb0dfee023e5a021ad747e56688179bb
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2026-08-15 14:48:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,10 +53,10 @@ class Config:
         self.COOKIES_URL: List[str] = self._parse_cookies()
 
         # Images
-        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://graph.org/file/8d492699bd4337068e8b0-069afe8a3596204342.mp4")
-        self.PING_IMG: str = getenv("PING_IMG", "https://graph.org/file/8d492699bd4337068e8b0-069afe8a3596204342.mp4")
-        self.START_IMG: str = getenv("START_IMG", "https://graph.org/file/8d492699bd4337068e8b0-069afe8a3596204342.mp4")
-        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://graph.org/file/8d492699bd4337068e8b0-069afe8a3596204342.mp4")
+        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://i.ibb.co/MkS4qhSh/anime.jpg")
+        self.PING_IMG: str = getenv("PING_IMG", "https://i.ibb.co/MkS4qhSh/anime.jpg")
+        self.START_IMG: str = getenv("START_IMG", "https://i.ibb.co/MkS4qhSh/anime.jpg")
+        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://i.ibb.co/MkS4qhSh/anime.jpg")
         
         # Moderation
         self.EXCLUDED_USERNAMES: List[str] = getenv("EXCLUDED_USERNAMES", "").split()
